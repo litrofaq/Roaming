4d34c6d1efe420aa7094d85c70acc62e9e8f70cf
litrofaq <litrofaq@yandex.ru>
2022-04-19 11:17:47 +0500

    add test: Ввод полной валидной формы и переход на второй шаг

--- Roaming/CounterAgents.cs
+++ Roaming/CounterAgents.cs
@@ -10,4 +10,16 @@ public class CounterAgents
     public string email;
     public string telephone;
 
+
+    public CounterAgents(string title = null, string inn = null, string kpp = null, int label = default,
+        string fullName = null, string email = null, string telephone = null)
+    {
+        this.title = title ?? throw new ArgumentNullException(nameof(title));
+        this.inn = inn ?? throw new ArgumentNullException(nameof(inn));
+        this.kpp = kpp ?? throw new ArgumentNullException(nameof(kpp));
+        this.label = label;
+        this.fullName = fullName ?? throw new ArgumentNullException(nameof(fullName));
+        this.email = email ?? throw new ArgumentNullException(nameof(email));
+        this.telephone = telephone ?? throw new ArgumentNullException(nameof(telephone));
+    }
 }
--- Roaming/RoamingQA.cs
+++ Roaming/RoamingQA.cs
@@ -7,9 +7,27 @@ namespace Roaming;
 
 public class RoamingQA
 {
+    private CounterAgents myFullFields = new CounterAgents("ООО 'Сладкая сказка'", "1839001744", "165701001",
+        1, "Иванов Сидор Петрович", "ivanov@test.ru","89051234567");
+    
+    
     private ChromeDriver driver;
     private string urlRoaming = "https://qa-course.kontur.host/roaming";
+    
+    // первый экран
+    private By titleLokator = By.Id("Company");
+    private By innLokator = By.Id("Inn");   
+    private By kppLokator = By.Id("Kpp");
+    private By nameLokator = By.Id("Name");
+    private By emailLokator =By.Id("Liame");
+    private By phoneLokator = By.Id("Phone");
     private By button1Lokator = By.ClassName("js-roaming-next-step-button");
+
+
+    // Второй экран
+    
+    private By scanTextLokator = By.Id("file-upload-text");
+    
     
     [SetUp]
     public void SetUp()
@@ -22,7 +40,8 @@ public class RoamingQA
     }
     
     [Test]
-    public void _blankTest()
+    // Ввод полной валидной формы и переход на второй шаг
+    public void truFullFirstForm_jumpStep2()
     {
         // Перейти по урлу
         driver.Navigate().GoToUrl(urlRoaming);
@@ -30,14 +49,28 @@ public class RoamingQA
         // Ожидать 10 секунд появления кнопки(загрузка страницы окончена)
         WebDriverWait waitVisibleInput = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         waitVisibleInput.Until(e => e.FindElement(button1Lokator));
+        
+        // Ввести все поля
+        driver.FindElement(titleLokator).SendKeys(myFullFields.title);
+        driver.FindElement(innLokator).SendKeys(myFullFields.inn);
+        driver.FindElement(kppLokator).SendKeys(myFullFields.kpp);
+        driver.FindElement(nameLokator).SendKeys(myFullFields.fullName);
+        driver.FindElement(emailLokator).SendKeys(myFullFields.email);
+        driver.FindElement(phoneLokator).SendKeys(myFullFields.telephone);
+        //driver.FindElement().SendKeys();
+
+        
+        
         // Кликнуть по кнопке
-        //driver.FindElement(buttonSendMeLokator).Click();
+        driver.FindElement(button1Lokator).Click();
 
-
+        // Ожидать 10 секунд появления второго состояния
+        WebDriverWait waitVisible2screen = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        waitVisibleInput.Until(e => e.FindElement(scanTextLokator));
         Assert.Multiple(() =>
         {
-            Assert.IsTrue(driver.FindElement(button1Lokator).Displayed);//,
-                //$"\tОжидалась успешная валидация емэйла домена третьего уровня '{email3LevelDomain}'\n" +
+            Assert.IsTrue(driver.FindElement(scanTextLokator).Displayed);
+                //$"\tОжидалась успешная валидация и переход на второе состояние '");//{email3LevelDomain}'\n" + 
                 //$"Фактически ошибка валидации емэйл: '{driver.FindElement(errorFormLokator).Text}'");
         });
     }
